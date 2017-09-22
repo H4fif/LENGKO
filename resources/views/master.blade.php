c4cf1f02ecd82aee0384fab66079f599c9c98fdd
master <gurisadevs@gmail.com>
2017-09-22 22:24:57 +0700

    Lengko coming soon..

---
+++ resources/views/master.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <meta content="IE=edge" http-equiv="X-UA-Compatible" />
+  <meta content="width=device-width,initial-scale=1" name="viewport" />
+
+  <link href="<?php echo url()->full(); ?>/assets/bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css">
+  <title>LENGKO</title>
+</head>
+<body>
+  <header>
+    <nav>
+
+    </nav>
+
+  </header>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-offset-4 col-md-4">
+        <h1 class="text-center">
+          Hello World!<br />
+          <small>Lengko was here..</small>
+        </h1>
+      </div>
+    </div>
+  </div>
+
+  <footer>
+
+  </footer>
+
+  <script src="<?php echo url()->full(); ?>/assets/jquery/jquery.js" type="text/javascript"></script>
+  <script src="<?php echo url()->full(); ?>/assets/bootstrap/js/bootstrap.js" type="text/javascript"></script>
+</body>
+</html>
